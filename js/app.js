0dca5aeefe83c5c7b8495491cd2b040991feb917
Lee Jiyoung <lizzie.yyy@gmail.com>
2023-05-31 22:15:57 +0900

    피드백 반영1 - for문 부분 함수로 빼기

--- js/app.js
+++ js/app.js
@@ -19,11 +19,11 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=ko&page=1', options
     .then((response) => response.json())
     .then((response) => {
         movies = Object.entries(response['results']);
-        loadData();
+        loadData(movies);
     })
     .catch((err) => console.error(err));
 
-let loadData = () => {
+let loadData = (movies) => {
     $movieList.textContent = '';
     for (const movie of movies) {
         let { id, title, overview, poster_path, vote_average } = movie[1];
@@ -58,34 +58,12 @@ let searchData = (searchKeyword) => {
     });
     if (!filteredMovies.length) return alert('해당하는 영화가 없습니다');
 
-    $movieList.textContent = '';
-    for (const movie of filteredMovies) {
-        let { id, title, overview, poster_path, vote_average } = movie[1];
-        let index = Number(movie[0]) + 1;
-        let poster_img = 'https://image.tmdb.org/t/p/w500/' + poster_path;
-
-        let temp_html = `<li>
-            <a href="#this" class="figure" onclick="alert('${id}')">
-                <div class="thumb">
-                    <span class="num">${index}</span>
-                    <img src="${poster_img}" alt="영화 포스터" />
-                    <div class="cover">
-                        <span>${overview}</span>
-                    </div>
-                </div>
-                <div class="figcaption">
-                    <strong>${title}</strong>
-                    <span class="rating">⭐${vote_average}</span>
-                </div>
-            </a>
-        </li>`;
-        $movieList.insertAdjacentHTML('beforeend', temp_html);
-    }
+    loadData(filteredMovies);
 };
 
 let clearInput = () => {
     $searchInput.value = '';
-    loadData();
+    loadData(movies);
 };
 
 let keyboardSearch = (e) => {
@@ -136,5 +114,3 @@ let delRecentKeyword = (target) => {
 
     loadRecentKeyword();
 };
-
-// document.querySelectorAll('')
